5f294f3b88a3ba8bbe028fa35e1ce028ca61896e
BeyForge X <seraphinetheghost@protonmail.com>
2026-09-05 21:26:48 -0700

    PWA fix (real manifest+sw), remove combo card, modals replace prompt/confirm, search filters on dropdowns

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'beyforge-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './data.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
